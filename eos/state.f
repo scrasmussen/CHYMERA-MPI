2745b0079df7efbd71881ba4b938c26a5360947a
William Dumas <dumas2@uoregon.edu>
2014-10-16 15:36:31 -0700

    changed the equation of state since we are doing constant gamma runs.

--- eos/state.f
+++ eos/state.f
@@ -20,15 +20,22 @@ C$OMP DO SCHEDULE(STATIC) PRIVATE(mu)
          DO K=1,KMAX2
             DO J=jstart,jmax2
 
-              call get_gamma2(eps(J,K,L),rho(J,K,L),tempk(J,K,L),
-     &             mu,gamma1(J,K,L))
-              p(J,K,L) = bkmpcgs*rho(J,K,L)*tempk(J,K,L)*rhoconv
-     &            / (mu*pconv)
-
+c              call get_gamma(eps(J,K,L),rho(J,K,L),tempk(J,K,L),
+c     &             mu,gamma)
+c              p(J,K,L) = bkmpcgs*rho(J,K,L)*tempk(J,K,L)*rhoconv
+c     &            / (mu*pconv)
+               p(j,k,l) = eps(j,k,l)*(gamma - one)
             end do
          ENDDO
       ENDDO
 C$OMP END DO
+c        do l =1,8
+c           do k = 1,4
+c              do j = 1,7
+c        write(97,*) p(j,k,l), (gamma-1)*eps(j,k,l)
+c              end do
+c           end do
+c        end do
 
       RETURN
       END
