6a49c98281e367667f981c86528cf7ca3e7167ac
William Dumas <dumas2@uoregon.edu>
2014-10-20 14:33:51 -0700

    Changed the indirect potential to use the position as calculated by wiggle.F (which evolves the position based on gravitational force), rather than usiing indirect routine which assumes position of star is such that center of mass of the system remains fixed.

--- pot/ExternalPot.f
+++ pot/ExternalPot.f
@@ -1,12 +1,12 @@
-      subroutine ExternalPot()
+      subroutine ExternalPot(ITSTEP)
       implicit none
 #include "hydroparam.h"
 #include "globals.h"
 #include "units.h"
-      integer::J,K,L
+
+      integer::J,K,L,ITSTEP
       real*8::thisMass,omegaBin2,rcom,theta,x,y
       real*8::rBin,thetaBin,thisTime,omegaBin,massDisk,muMass,rdist
-
       thisTime=time
 
 #if BINARY>0
@@ -15,32 +15,45 @@
       omegaBin2=omegabin**2
       rcom=rbin-rbin*mass_star/muMass
 #endif
+! Use the position of the star as calculated by wiggle.F.
+             indirectx = rpstar*cos(phi_star)
+             indirecty = rpstar*sin(phi_star)
 
 #if INDIRECT>0
 !$OMP DO SCHEDULE(STATIC) REDUCTION(+:indirectx,indirecty)
+c Then calculate the position of the star is such that the center of
+c mass of the system remains zero.
       do L=1,LMAX
         do K=2,KMAX1
           do J=JMIN,JMAX1
              thisMass=rho(J,K,L)*rhf(J)*rof3n*zof3n*two*dtheta ! take into account both sides
+
              indirectx=indirectx+thisMass*cos((dble(L-1)+half)*dtheta)
-     &                *rhf(J)/(sqrt(rhf(J)*rhf(J)+zhf(K)*zhf(K)))**3
+     &                *rhf(J)
              indirecty=indirecty+thisMass*sin((dble(L-1)+half)*dtheta)
-     &                *rhf(J)/(sqrt(rhf(J)*rhf(J)+zhf(K)*zhf(K)))**3
+     &                *rhf(J)
+
+
           enddo
         enddo
       enddo
 !$OMP ENDDO
+             indirectx = -indirectx/mass_star
+             indirecty = -indirecty/mass_star
 #endif
- 
+
 !$OMP DO SCHEDULE(STATIC) PRIVATE(theta,rdist,x,y)
             do l=1,lmax
        theta=(dble(L)-half)*dtheta
                 do k=1,kmax2
                   do j=1,jmax2
                      starphi(j,k,l) = -(mass_star+tmassacc)
-     &                       /(sqrt(rhf(J)*rhf(J)+zhf(K)*zhf(K)))
+     & /sqrt(rhf(J)*rhf(J)+zhf(K)*zhf(K) + indirectx*indirectx
+     &  + indirecty*indirecty - two*rhf(J)
+     & *(cos((dble(L-1)+half)*dtheta)*indirectx 
+     & + sin((dble(L-1)+half)*dtheta)*indirecty))    
                      phi(j,k,l)= phi(j,k,l)+starphi(j,k,l) ! ELIMINATED diskphi. ACB.
-
+                    
 #if BINARY>0
        !!!!!!!!!!!!!!
        !! Add Binary!
@@ -58,18 +71,28 @@
 #endif
 
 #if INDIRECT>0
-       !!!!!!!!!!!!!!!!
-       !! Add Indirect!
-       !!!!!!!!!!!!!!!!
-                     phi(J,K,L)=phi(J,K,L)
-     &+indirectx*rhf(J)*cos((dble(L-1)+half)*dtheta)
-     &+indirecty*rhf(J)*sin((dble(L-1)+half)*dtheta)
+c       !!!!!!!!!!!!!!!!
+c       !! Add Indirect!
+c       !!!!!!!!!!!!!!!!
+c                     phi(J,K,L)=phi(J,K,L)
+c     &+indirectx*rhf(J)*cos((dble(L-1)+half)*dtheta)
+c     &+indirecty*rhf(J)*sin((dble(L-1)+half)*dtheta)
+
+                     phi(j,k,l)=phi(j,k,l)
+     &+(indirectx2*rhf(J)+indirecty2*(dble(L-1)+half)*dtheta)
+     &+mass_star*(indirectx*rhf(J)
+     &*cos((dble(L-1)+half)*dtheta)/sqrt(rhf(J)**2+zhf(k)**2)**3
+     &+(indirecty2*rhf(J)-indirectx2*(dble(L-1)+half)*dtheta)
+     &+indirecty*rhf(J)
+     &*sin((dble(L-1)+half)*dtheta)/sqrt(rhf(j)**2+zhf(k)**2)**3
+     &+half*(indirectx**2+indirecty**2)/sqrt(rhf(J)**2+zhf(k)**2)**3)
+
 #endif 
+
                   enddo
                enddo
             enddo
 !$OMP END DO
-
       return
       end subroutine ExternalPot
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
