0f6b836a3164c1b50e0ba6308d342add8f44b12d
Craig Rasmussen <rasmus@uoregon.edu>
2014-10-31 10:27:52 -0700

    Final cleanup in moving changes back to Poisson solver.

--- tests/fftw/Fortran/test_DFT.F90
+++ tests/fftw/Fortran/test_DFT.F90
@@ -1,6 +1,6 @@
 program Test_DFT
   use params,  only : M, N, H, R, TH, PI, kind_r
-  use DFT, only : Initialize, Finalize, Transform, TransformFFTW
+  use DFT, only : Initialize, Finalize, Transform, TransformDirect
 
   implicit none
 
@@ -14,13 +14,14 @@ program Test_DFT
   print '(16(g10.3,1x))', "theta:", Th/(2*PI)
   print '(16(g10.3,1x))', "r:", R
 
-  call TransformFFTW
-
 !! test the direct O(N^2) algorithm
 !
-!  call Transform
+!  call TransformDirect
+
+!! test the FFTW algorithm
+!
+  call Transform
 
   call Finalize
-
 
 end program Test_DFT
